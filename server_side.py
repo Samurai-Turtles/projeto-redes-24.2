b08d8761ef1cb54415937dd0ffd564093e35c7a8
JWillianSl <jose.williansl@hotmail.com>
2025-04-10 22:38:07 -0300

    feat: add the tcp implementation for client requests

--- server_side.py
+++ server_side.py
@@ -1,23 +1,25 @@
-import socket 
+import socket
 import threading
 import configparser
 
 MAX_FILE_SIZE = 10240
+MAX_REQUEST_SIZE = 1024
 TCP_PORT = None
 UDP_PORT = None
-FILE_A = None
-FILE_B = None
+FILES_PATH = "./files/"  # TODO, ler de um config?
+FILES = {"FILE_A": None, "FILE_B": None}
+
 
 def load_config():
     global TCP_PORT, UDP_PORT, FILE_A, FILE_B
 
     config = configparser.ConfigParser()
     config.read("config.ini")
-    
+
     TCP_PORT = int(config["SERVER_CONFIG"]["TCP_PORT"])
     UDP_PORT = int(config["SERVER_CONFIG"]["UDP_PORT"])
-    FILE_A = config["SERVER_CONFIG"]["FILE_A"]
-    FILE_B = config["SERVER_CONFIG"]["FILE_B"]
+    FILES["FILE_A"] = config["SERVER_CONFIG"]["FILE_A"]
+    FILES["FILE_B"] = config["SERVER_CONFIG"]["FILE_B"]
 
 
 def process_udp_request(data: bytes):
@@ -40,28 +42,46 @@ def process_udp_request(data: bytes):
     """
     converted = data.decode("utf-8").split(",")
     converted[-1] = converted[-1].strip()
-    out = f"RESPONSE,TCP,{TCP_PORT},{converted[-1]}"    
+    out = f"RESPONSE,TCP,{TCP_PORT},{converted[-1]}"
 
     if "UDP" in converted:
         out = "ERROR,PROTOCOLO INVALIDO,,"
-    
-    if not (converted[-1] == FILE_A or converted[-1] == FILE_B):
+
+    if not (converted[-1] in FILES.values()):
         out = "ERROR,ARQUIVO INEXISTENTE,,"
+
+    return out
+
+
+def process_tcp_request(data: bytes):
+    converted = data.decode().split(",")
+    converted[-1] = converted[-1].strip()
+    out = None
+
+    if converted[0] != "get":
+        return out
+
+    with open(f"{FILES_PATH}{converted[-1]}", 'rb') as file:
+        out = file.read()
 
     return out
 
 
 def handle_tcp_client(conn, addr):
     print(f"TCP Client connected from {addr}")
-    
+
     with conn:
-        data = conn.recv(MAX_FILE_SIZE)
+        data = None
 
         while not data:
-            print(data.decode("utf-8"))
-            conn.sendall(data)
-            data = conn.recv(MAX_FILE_SIZE)
-    
+            data = conn.recv(MAX_REQUEST_SIZE)
+
+        processed_file = process_tcp_request(data)
+
+        if processed_file != None:
+            conn.sendall(processed_file)
+            print(f"File sent to {addr}.")
+
     print(f"TCP Client disconnected from {addr}")
 
 
@@ -76,7 +96,8 @@ def tcp_protocol():
 
     while True:
         conn, addr = tcp_sock.accept()
-        client_thread = threading.Thread(target=handle_tcp_client, args=(conn, addr))
+        client_thread = threading.Thread(
+            target=handle_tcp_client, args=(conn, addr))
         client_thread.daemon = True
         client_thread.start()
 
@@ -91,12 +112,14 @@ def udp_protocol():
     udp_sock.bind(("0.0.0.0", UDP_PORT))
 
     while True:
-        data, addr = udp_sock.recvfrom(1024)
-        
+        data, addr = udp_sock.recvfrom(MAX_REQUEST_SIZE)
+
         if not data:
             continue
-        
-        print(f"UDP Received from {addr}: {data.decode("utf-8")}")
+
+        protocol_message = data.decode("utf-8")
+        print(f"UDP Received from {addr}: {protocol_message}")
+
         client_request = process_udp_request(data)
         udp_sock.sendto(str.encode(client_request), addr)
 
@@ -121,7 +144,7 @@ def main():
 
     init_protocol(udp_protocol)
     init_protocol(tcp_protocol)
-    
+
     try:
         while True:
             pass
